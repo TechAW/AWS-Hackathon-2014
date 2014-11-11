b7d86ada37ba9f28f66480d08bbe6f8030b7beeb
Brian Conn <bcconn2112@gmail.com>
2014-11-11 14:12:31 -0500

    Updated config to allow Account controller.

--- grails-app/conf/Config.groovy
+++ grails-app/conf/Config.groovy
@@ -142,6 +142,7 @@ grails.plugin.springsecurity.controllerAnnotations.staticRules = [
 	'/**/css/**':						['permitAll'],
 	'/**/images/**':					['permitAll'],
 	'/**/favicon.ico':					['permitAll'],
-	'/home/**':							['permitAll']
+	'/home/**':							['permitAll'],
+	'/account/**':						['permitAll']
 ]
 
