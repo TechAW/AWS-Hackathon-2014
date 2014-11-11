4c9d4f1873c5e6cda2bddd721b82629d3c67b817
Jukka Dahlbom <jdahlbom@gmail.com>
2014-11-11 11:38:14 -0800

    Hook up twiliosender to notificationService

--- grails-app/services/org/atrocitywatch/NotificationService.groovy
+++ grails-app/services/org/atrocitywatch/NotificationService.groovy
@@ -4,8 +4,9 @@ import grails.transaction.Transactional
 
 @Transactional
 class NotificationService {
-	
+
 	def mailService
+	def twilioSender
 
     def notify(User user,subject,msg) {
       if (user.email!=null) {
@@ -15,7 +16,7 @@ class NotificationService {
 		  notifyBySMS(user,subject,msg)
 	  }
     }
-	
+
 	def notifyByEmail(User user,String subject,String msg) {
 		mailService.sendMail {
 			multipart false
@@ -25,7 +26,8 @@ class NotificationService {
 			html msg
 		}
 	}
-	
+
 	def notifyBySMS(User user,String subject,String msg) {
+		twilioSender.sendSMS(user.phone, msg);
 	}
 }
